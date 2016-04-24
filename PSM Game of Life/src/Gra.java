b0459d526889c00f165822327d542301445dccd1
Endrju <Endrju@DESKTOP-VM4B95A.fritz.box>
2016-04-24 15:51:11 +0200

    pierwsze zmiany

--- PSM Game of Life/src/Gra.java
+++ PSM Game of Life/src/Gra.java
@@ -1,4 +1,27 @@
 
 public class Gra {
+	private static boolean board[][];
+	private static boolean nextBoard[][];
+	private int dim;
+
+	public Gra(int sizeOfBoard) {
+		dim = sizeOfBoard;
+		board = new boolean[dim][dim];
+		nextBoard = new boolean[dim][dim];
+	}
+
+	public void init() {
+	}
+
+	public void readFile() {
+	}
+
+	public void writeToFile(boolean tab[][]) {
+	}
+
+	public boolean[][] nextIter() {
+
+		return null;
+	}
 
 }
